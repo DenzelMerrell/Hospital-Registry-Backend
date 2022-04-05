c50de07a863a624694611db03e7c91364fe1e8ac
DenzelMerrell <denzelmerrell@gmail.com>
2022-04-05 11:45:25 -0400

    added response headers

--- routes/patients.js
+++ routes/patients.js
@@ -13,7 +13,7 @@ router.get('/all', async (req, res) => {
     // Access-Control-Allow-Methods : GET, POST, OPTIONS
     // Access-Control-Allow-Headers : Origin, Content-Type, Accept
 
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
